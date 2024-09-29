1211348a86c64d9ec8cdf4b61c957513b410cdae
Liam Gilbey <liam.gilbey@gmail.com>
2024-09-29 21:03:31 +1300

    Including font awesome

--- frontend/src/components/Dashboard/Navbar.js
+++ frontend/src/components/Dashboard/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserDropDown from './User';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/Navbar.css'
 
 const Navbar = () => {
@@ -40,9 +42,7 @@ const Navbar = () => {
                             </span>
                         </div>
                         <div class="drowndown-icon">
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
-                            </svg>
+                            <FontAwesomeIcon icon={faUser} size="2x" />
                         </div>
                     </button>
                     {isDropdownOpen && (
